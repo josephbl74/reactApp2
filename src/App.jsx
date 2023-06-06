24fc4b71fdf756ba4df7f400f187eb4f4d50860b
josephbl74 <79768976+josephbl74@users.noreply.github.com>
2023-06-06 13:02:16 +0200

    3.0.0 - local storage working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { NewMarkerForm } from "./NewMarkerForm"
 import { MarkerList } from "./MarkerList"
 import "./styles.css"
 
 export default function App(){
 
-  const [markers, setMarkers]=useState([])
+  const [markers, setMarkers]=useState(() => {
+    const localValue=localStorage.getItem("MARKERS")
+    if(localValue == null) return []
+    return JSON.parse(localValue)
+  })
+
+  //useEffect() runs the described function every time the array passed sa second property changes
+  // allows exporting data to our local storage
+  useEffect(() => {
+    localStorage.setItem("MARKERS", JSON.stringify(markers))
+  }, [markers])
 
   function addMarker(title){
     setMarkers((currentMarkers) => [
